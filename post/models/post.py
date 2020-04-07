c6eba4f7d51123c33d014a543da36a1cbfb0c010
srivastava9 <krishu4626@gmail.com>
2020-04-07 21:00:55 +0530

    post is_expired made

--- post/models/post.py
+++ post/models/post.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save
-
+import datetime
 from post.utils import unique_slug_generator
 
 from utilities.models import TimestampedModel
@@ -55,6 +55,14 @@ class AbstractPost(TimestampedModel):
         default=False,
         verbose_name="Verified"
     )
+
+    @property
+    def is_expired(self):
+        """
+        Return whether post has expired or not
+        True /False 
+        """
+        return datetime.date.today() > self.post_expiry_date
 
     class Meta:
         """
