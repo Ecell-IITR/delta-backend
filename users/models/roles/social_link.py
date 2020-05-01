473a58c831d290cb74dc5a2d48246028bc58440b
Tushar <tvarshney@me.iitr.ac.in>
2020-05-01 08:35:06 +0530

    [feat] Update profile API refactored

--- users/models/roles/social_link.py
+++ users/models/roles/social_link.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
+
 from utilities.models import Website
 
 class SocialLink(models.Model):
@@ -11,7 +12,7 @@ class SocialLink(models.Model):
     )    
 
     class Meta:
-        verbose_name_plural = "Socila Links"
+        verbose_name_plural = "Social Links"
     
     def __str__(self):
         return self.website.name
--- users/views/person.py
+++ users/views/person.py
@@ -6,6 +6,9 @@ from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
 from users.constants import GET_ROLE_TYPE
+from users.models import SocialLink
+
+from utilities.models import Branch, Website
 
 from users.serializers import (
     StudentSerializer,
@@ -63,19 +66,121 @@ class SelfProfile(generics.RetrieveUpdateAPIView):
         )
 
     def update(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer_class()(
-            queryset,
-            data=request.data,
-        )
-        serializer.is_valid(
-            raise_exception=True
-        )
-        self.perform_update(
-            serializer
-        )
+        user = request.user
+        data = request.data
 
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-        )
+        if user.student_profile:
+            user_profile = user.student_profile
+
+            first_name = data.get('first_name') or None
+            last_name = data.get('last_name') or None
+            branch = data.get('branch') or None
+            phone_number =  data.get('phone_number') or None
+            enrollment_number = data.get('enrollment_number') or None
+            course = data.get('course') or None
+            year = data.get('year') or None
+            social_link = data.get('social_link') or None
+            interest = data.get('interest') or None
+            bio = data.get('bio') or None
+            achievements = data.get('achievements') or None
+            availability_status = data.get('availability_status') or None
+            
+            if first_name:
+                first_name = first_name.strip()
+                user_profile.first_name = first_name
+            
+            if last_name:
+                last_name = last_name.strip()
+                user_profile.last_name = last_name
+
+            if phone_number:
+                phone_number = phone_number.strip()
+                user_profile.phone_number = phone_number
+
+            if branch:
+                try:
+                    branch = get_object_or_404(Branch, name=branch)
+                except Branch.DoesNotExist:
+                    return Response({'error_message': 'Branch not found'}, status=status.HTTP_400_BAD_REQUEST)
+                user_profile.branch = branch
+            
+            if enrollment_number:
+                enrollment_number = enrollment_number.strip()
+                user_profile.enrollment_number = enrollment_number
+            
+            if course:
+                course = course.strip()
+                user_profile.course = course
+            
+            if year:
+                year = year.strip()
+                user_profile.year = year
+            
+            if social_link:
+                try:
+                    website = get_object_or_404(Website, name=social_link.get('website_name') or None)
+                except Website.DoesNotExist:
+                    return Response({'error_message': 'Website not found'}, status=status.HTTP_400_BAD_REQUEST)
+                
+                profile_url = social_link.get('profile_url') or None
+                
+                if profile_url:
+                    if user_profile.social_links.filter(profile_url=profile_url, website=website).exists():
+                        return Response({'error_message': 'Already profile added for this website'}, status=status.HTTP_400_BAD_REQUEST)
+                    
+                    user_profile.social_links.create(website=website, profile_url=profile_url)
+
+                else:
+                    return Response({'error_message': 'Profile url not found!'}, status=status.HTTP_400_BAD_REQUEST)
+
+            if interest:
+                interest = interest.strip()
+                user_profile.interest = interest
+                        
+            if bio:
+                bio = bio.strip()
+                user_profile.bio = bio   
+
+            if achievements:
+                achievements = achievements.strip()
+                user_profile.achievements = achievements         
+                
+            if availability_status:
+                user_profile.availability_status = availability_status
+
+            user_profile.save()
+
+            return Response(status=status.HTTP_200_OK)
+
+        elif user.company_profile:
+            user_profile = user.company_profile
+
+            company_domain = data.get('availability_status') or None
+            phone_number = data.get('availability_status') or None
+            category_of_company = data.get('availability_status') or None
+            team_size = data.get('availability_status') or None
+            address = data.get('availability_status') or None
+
+            if company_domain:
+                company_domain = company_domain.strip()
+                user_profile.company_domain = company_domain  
+
+            if phone_number:
+                phone_number = phone_number.strip()
+                user_profile.phone_number = phone_number  
+            
+            if category_of_company:
+                category_of_company = category_of_company.strip()
+                user_profile.category_of_company = category_of_company  
+            
+            if team_size:
+                team_size = team_size.strip()
+                user_profile.team_size = team_size  
+            
+            if address:
+                address = address.strip()
+                user_profile.address = address  
+            
+            user_profile.save()
+            
+            return Response(status=status.HTTP_200_OK)
