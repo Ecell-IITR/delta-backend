dbe649d4140a49bb026ba09b9b0a780728aebd92
Tushar <tvarshney@me.iitr.ac.in>
2020-06-17 21:58:08 +0530

    [feat] Bug fixes and organization api changes

--- users/views/organization.py
+++ users/views/organization.py
@@ -5,6 +5,8 @@ from rest_framework import generics
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
+from common.field_choices import USER_FIELD_CHOICES
+
 from users.constants import GET_ROLE_TYPE
 from users.serializers import OrganizationListSerializer
 from users.models import Company
@@ -20,9 +22,13 @@ class OrganizationList(generics.ListAPIView):
         return context
 
     def get_queryset(self, *args, **kwargs):
+        user = self.request.user
         list_type = self.request.GET.get('list_type')
+        queryset = []
         if list_type == 'all':
             queryset = Company.objects.all()
-        else:
-            queryset = []
+        elif list_type == 'following-list':
+            company_user_list = user.action_by_person.filter(action_on_person__role_type=GET_ROLE_TYPE.COMPANY, 
+                action=USER_FIELD_CHOICES.FOLLOW).values_list('action_on_person__username', flat=True)
+            queryset = Company.objects.filter(person__username__in=company_user_list)
         return queryset
