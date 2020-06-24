54e99cb48f215a8c0d081e862503c1d0b60e737a
Tushar <tvarshney@me.iitr.ac.in>
2020-06-25 00:56:57 +0530

    [fix] Bug fixes

--- delta/settings/__init__.py
+++ delta/settings/__init__.py
@@ -5,6 +5,8 @@ from .base import *
 env = os.environ.get('ENVIRONMENT')
 print('Checking working environment: ', env)
 if env == 'production':
+    print('Importing production settings...')
     from .deployment import *
 else:
+    print('Importing development settings...')
     from .development import *
