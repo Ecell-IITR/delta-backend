3c58c19698eef89b97f3c80bb8a8b61a60293ec9
Tushar <tvarshney@me.iitr.ac.in>
2020-06-25 13:50:55 +0530

    [fix] Bug fixes

--- delta/settings/__init__.py
+++ delta/settings/__init__.py
@@ -1,3 +1,1 @@
 import os
-
-from .base import *
--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'delta.settings')
+    if os.environ.get('ENVIRONMENT') == 'production':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'delta.settings.deployment')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'delta.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
