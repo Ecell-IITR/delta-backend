ca73770b832e770be3c1963384bf2dd70e50010e
Darkshredder <srivastavayash58@gmail.com>
2021-10-15 01:24:31 +0530

    Adds stipend range and social_links website api

--- utilities/urls.py
+++ utilities/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from utilities.views import SkillsAPIView, SkillAddAPI, SkillRemoveAllAPI, SkillRemoveAPI, LocationsListAPI, TagsListAPI
+from utilities.views import SkillsAPIView, SkillAddAPI, SkillRemoveAllAPI, SkillRemoveAPI, LocationsListAPI, SocialLinksWebsitesAPIView, TagsListAPI
 
 urlpatterns = [
     path(r'skills/', SkillsAPIView.as_view(), name="skills-list"),
+    path(r'social_links_websites/', SocialLinksWebsitesAPIView.as_view(), name="social-links-list"),
     path(r'skills/add/<slug:slug>/', SkillAddAPI.as_view(), name="skill-add"),
     path(r'skills/remove/<slug:slug>/', SkillRemoveAPI.as_view(), name="skill-remove"),
     path(r'skills/remove-all/', SkillRemoveAllAPI.as_view(), name="skills-remove-all"),
